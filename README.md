37c0eef39f19bcc5c74d6bc2106db09ddc01df2e
Brandon Liang <54skyxenon@gmail.com>
2020-06-18 02:44:19 -0700

    deployed backend

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![readme-banner.png](./assets/readme-banner.png)
+![readme-banner.png](./client/src/assets/topbanner.jpeg)
 
 Max Hayashi and I wrote a full-stack web application to investigate factors affecting housing prices in the
 Greater Boston area. This is our final project for CS3200: Database Design.
@@ -10,36 +10,26 @@ backend is written using the Django MVC framework, connected to a MySQL AWS inst
 You need to first add the `.env` file containing the secret key and database connection information to run the backend. 
 Just email me or Max asking for it. Also, ensure you are using Python 3 when running the backend.
 
-To start the frontend server, cd to `client/` and then you can:
-
-### `yarn start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
 To start the backend server, stay in this directory and then:
 
 ### `pip install -r requirements.txt`
 
-Installs the packages needed for the Django server to start.
+Installs the packages needed for the Flask server to start.
+
+
+To start the frontend server, cd to `client/` and then you can:
+
+### `yarn start`
+
+Runs the app in the development mode. Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits. You will also see any lint errors in the console.
+
+### `yarn test`
+
+Launches the test runner in the interactive watch mode. See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `yarn build`
+
+Builds the app for production to the `build` folder. It correctly bundles React in production mode and optimizes the build for the best performance. We use this when running `netlify deploy --prod`.
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ app/main.py
@@ -0,0 +1,102 @@
+# A realllyy bad backend written by Brandon
+
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import simplejson as json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+app.config['MYSQL_DB'] = os.getenv('DB_NAME')
+app.config['MYSQL_USER'] = os.getenv('DB_USERNAME')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD')
+app.config['MYSQL_HOST'] = os.getenv('DB_ENDPOINT')
+app.config['MYSQL_PORT'] = int(os.getenv('DB_PORT'))
+
+mysql = MySQL(app)
+
+# https://stackoverflow.com/a/43796849
+def toJson(cur):
+    # this will extract row headers
+    row_headers = [x[0] for x in cur.description]
+    rv = cur.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return json.dumps(json_data)
+
+# avg subway distance from new constructions
+@app.route('/subway', methods=['GET'])
+def subway():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        select
+            neighborhood.neighborhood_id,
+            neighborhood.name,
+            avg(distance),
+            commuting_subway / working_population * 100 as 'percent_commuting_subway'
+        from neighborhood join zip_neighborhood using (neighborhood_id)
+        join zip using (zip_id)
+        join new_construction_permits using (zip_id)
+        join closest_subway_stop using (new_construction_permit_id)
+        left join subway_stats using (neighborhood_id)
+        group by neighborhood.name
+        order by percent_commuting_subway desc;
+    ''')
+    return toJson(cur)
+
+# amount of low income housing compared to population
+@app.route('/loinc', methods=['GET'])
+def loinc():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        select 
+            neighborhood.name, 
+            sum(income_restricted_units) as 'total_income_restricted_units', 
+            working_population
+        from neighborhood join zip_neighborhood using (neighborhood_id)
+        join zip using (zip_id)
+        join low_income_housing using (zip_id)
+        left join subway_stats using (neighborhood_id)
+        group by neighborhood.name
+        order by total_income_restricted_units desc;
+    ''')
+    return toJson(cur)
+
+# which neighborhoods are building the most apartments?
+@app.route('/most-apartments', methods=['GET'])
+def mostApartments():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        select 
+            neighborhood.name, 
+            count(*) as 'num_apartments'
+        from neighborhood join zip_neighborhood using (neighborhood_id)
+        join zip using (zip_id)
+        join new_construction_permits using (zip_id)
+        where occupancy = 'Multi'
+        group by neighborhood.name
+        order by num_apartments desc;
+    ''')
+    return toJson(cur)
+
+# will new apartments be closer to subways than other buildings?
+@app.route('/new-apartments', methods=['GET'])
+def newApartments():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        select 
+            "Apartments", 
+            avg(distance)
+        from new_construction_permits join closest_subway_stop using (new_construction_permit_id)
+        where occupancy = 'Multi'
+        union
+        select 
+            "Other buildings", 
+            avg(distance)
+        from new_construction_permits join closest_subway_stop using (new_construction_permit_id)
+        where occupancy != 'Multi';
+    ''')
+    return toJson(cur)
